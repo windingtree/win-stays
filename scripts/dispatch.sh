a9d13e5dea7c92eee2ce9fdd33685795881d4f19
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-05-17 12:51:28 +0200

    fix: fixed dispatch workflow

--- scripts/dispatch.sh
+++ scripts/dispatch.sh
@@ -8,5 +8,5 @@ curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: Bearer $GITHUB_KEY" \
-  https://api.github.com/repos/kostysh/win/dispatches \
+  https://api.github.com/repos/windingtree/win-stays/dispatches \
   -d "{\"event_type\":\"release_to_npm\", \"client_payload\": {\"packageName\": \"$1\"}}"
