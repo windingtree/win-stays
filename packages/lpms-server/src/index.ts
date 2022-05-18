ae6a0a550a024b0235f70456b61ae778ee3fa5dc
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-05-18 18:10:39 +0200

    feat(lpms-server): added feature of the default manager user creation on start

--- packages/lpms-server/src/index.ts
+++ packages/lpms-server/src/index.ts
@@ -1,4 +1,32 @@
+import { AppRole } from '../src/types';
 import ServerService from './services/ServerService';
+import UserService from './services/UserService';
 import { port } from './config';
 
-new ServerService(port).start();
+process.on('unhandledRejection', error => {
+  console.log(error);
+  process.exit(1);
+});
+
+const main = async (): Promise<ServerService> => {
+  const server = new ServerService(port);
+
+  const userService = new UserService();
+  const users = await userService.getAllUsers();
+
+  if (users.length === 0) {
+    await userService.createUser(
+      'manager',
+      'winwin',
+      [AppRole.MANAGER]
+    );
+  }
+
+  return server.start();
+};
+
+export default main()
+  .catch(error => {
+    console.log(error);
+    process.exit(1);
+  });
