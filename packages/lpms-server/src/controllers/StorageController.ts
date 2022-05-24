67e7a6ffd956279415d5fb0aa3fbb44632a828f7
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-05-24 11:41:20 +0200

    feat(lpms-server): added signing of metadata in the storage contraoller

--- packages/lpms-server/src/controllers/StorageController.ts
+++ packages/lpms-server/src/controllers/StorageController.ts
@@ -1,16 +1,45 @@
 import type { NextFunction, Request, Response } from 'express';
-import { proto } from '@windingtree/videre-sdk';
+import type { TypedDataDomain } from '@ethersproject/abstract-signer';
+import type { Wallet } from 'ethers';
+import { promises } from 'fs';
+import { ServiceProviderData } from '@windingtree/stays-models/dist/cjs/src/proto/storage';
+import { utils as vUtils, eip712 } from '@windingtree/videre-sdk';
 import IpfsApiService from '../services/IpfsApiService';
+import walletService from '../services/WalletService';
 import { web3StorageKey } from '../config';
+const { readFile } = promises;
+
+export type ServiceProviderDataUnsigned = Omit<ServiceProviderData, 'signature'>;
 
 export class StorageController {
+  public async signMetadata(file: Express.Multer.File, signer: Wallet): Promise<Uint8Array> {
+    const domain: TypedDataDomain = {
+      name: "stays",
+      version: "1",
+      chainId: 100
+    };
+
+    const fileBuffer = await readFile(file.path);
+    const serviceProviderData = ServiceProviderData.fromBinary(fileBuffer) as ServiceProviderData;
+
+    const signedMessage = await vUtils.createSignedMessage(
+      domain,
+      eip712.storage.ServiceProviderData,
+      serviceProviderData,
+      signer
+    );
+
+    return ServiceProviderData.toBinary(signedMessage);
+  }
+
   public async uploadFile(req: Request, res: Response, next: NextFunction) {
     try {
-      const file = req.file as Express.Multer.File;
-      if (!file) {
+      const multerFile = req.file as Express.Multer.File;
+      if (!multerFile) {
         return next(new Error('File not uploaded'));
       }
       const storage = new IpfsApiService(web3StorageKey);
+      const file = await IpfsApiService.getFileFromMulter(multerFile);
       const result = await storage.deployFilesToIpfs([file]);
       return res.json(result);
     } catch (e) {
@@ -20,17 +49,21 @@ export class StorageController {
 
   public async uploadMetadata(req: Request, res: Response, next: NextFunction) {
     try {
-      const file = req.file as Express.Multer.File;
-      if (!file) {
+      const multerFile = req.file as Express.Multer.File;
+      if (!multerFile) {
         return next(new Error('File not uploaded'));
       }
 
-      // Validation of incoming data
-
-      // Creation of signed container
+      const signer = await walletService.getWalletByIndex(0); // 0 - API role
+      const signedMetadata = await this.signMetadata(multerFile, signer);
 
       const storage = new IpfsApiService(web3StorageKey);
+      const file = IpfsApiService.getFileFromBuffer(
+        signedMetadata,
+        multerFile.originalname
+      );
       const result = await storage.deployFilesToIpfs([file]);
+
       return res.json(result);
     } catch (e) {
       next(e);
