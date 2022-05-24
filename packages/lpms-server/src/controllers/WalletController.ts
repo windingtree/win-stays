3baebf6c35e44a99ba4025b738f1bcb6c4fd7af6
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-05-24 11:41:20 +0200

    feat(lpms-server): added signing of metadata in the storage contraoller

--- packages/lpms-server/src/controllers/WalletController.ts
+++ packages/lpms-server/src/controllers/WalletController.ts
@@ -1,9 +1,8 @@
-import WalletService from '../services/WalletService';
+import walletService from '../services/WalletService';
 import { Request, Response } from 'express';
 
 export class WalletController {
   public async getWallets(req: Request, res: Response): Promise<Response> {
-    const walletService = new WalletService();
     const accountsList = await walletService.getWalletAccounts();
 
     return res.json(accountsList);
