040c21d137149b5af11c72a1bb6f23ebc1e32676
bobetbat <b.bobetayev.97@gmail.com>
2022-06-07 16:38:54 +0200

    feat(stays): mapbox and search components

--- packages/stays/src/pages/Home.tsx
+++ packages/stays/src/pages/Home.tsx
@@ -1,18 +1,25 @@
+import type { LatLngExpression } from "leaflet";
 import { useAppState } from '../store';
 import { PageWrapper } from './PageWrapper';
 import { Search } from '../components/Search';
+import { MapBox } from '../components/MapBox';
+import { Box } from 'grommet';
+import { useState } from 'react';
+
+const defaultCenter: LatLngExpression = [51.505, -0.09]
 
 export const Home = () => {
   const { isConnecting } = useAppState();
+  const [center, setCenter] = useState<LatLngExpression>(defaultCenter)
 
   return (
     <PageWrapper>
       {!isConnecting &&
-        <div>
-          Hi!!!
-        </div>
+        <Box>
+          <Search onSubmit={setCenter}/>
+          <MapBox center={center} />
+        </Box>
       }
-      <Search />
     </PageWrapper>
   );
 };
